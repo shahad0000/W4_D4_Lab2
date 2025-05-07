05df23104ccd38cfd83aca7ac999af50bbb201c4
shahad0000 <81360310+shahad0000@users.noreply.github.com>
2025-05-07 13:50:20 +0300

    update

--- main.js
+++ main.js
@@ -2,21 +2,25 @@ let people = [
     {
         name: "Sara",
         age: "33",
+        gender: "female",
         img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg"
     },
     {
         name: "Mohammed",
         age: "23",
+        gender: "male",
         img: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1200"
     },
     {
         name: "Nour",
         age: "30",
+        gender: "female",
         img: "https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&w=1200"
     },
     {
         name: "Ali",
         age: "53",
+        gender: "male",
         img: "https://images.pexels.com/photos/834863/pexels-photo-834863.jpeg?auto=compress&cs=tinysrgb&w=1200"
     }
 ]
@@ -31,7 +35,7 @@ people.map(person => {
     card.style.marginBottom = "2rem";
     card.style.paddingTop = "2rem";
     card.style.textAlign = "center";
-
+    person.gender === "female" ? card.style.backgroundColor = "pink": card.style.backgroundColor = "lightBlue"
 
     let pImg = document.createElement("img");
     let name = document.createElement("h2");
